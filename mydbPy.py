c544dc1dbbcd81c867f268ceecaa6cda531d963c
Mattia <mta19@hotmail.it>
2023-05-05 16:33:46 +0200

    add file per creazione tabella

---
+++ mydbPy.py
@@ -0,0 +1,39 @@
+import psycopg2
+from psycopg2 import Error
+
+try:
+    # Connect to an existing database
+    connection = psycopg2.connect(user="unicorn_user",
+                                  password="magical_password",
+                                  host="127.0.0.1",
+                                  port="5433",
+                                  database="training")
+    cursor = connection.cursor()
+    #creiamo tabella
+    create_table_query = '''CREATE TABLE mobile
+              (ID INT PRIMARY KEY     NOT NULL,
+              MODEL           TEXT    NOT NULL,
+              PRICE         REAL); '''
+    cursor.execute(create_table_query)
+    connection.commit()
+    print("Table created successfully in PostgreSQL ")
+
+    #inseriamo dati
+    insert_query = """ INSERT INTO mobile (ID, MODEL, PRICE) VALUES (1, 'Iphone12', 1100)"""
+    cursor.execute(insert_query)
+    connection.commit()
+    #visualiziamo con SELECT
+    cursor.execute("SELECT * from mobile")
+    record = cursor.fetchall()
+    print("Result ", record)
+
+
+
+
+except (Exception, Error) as error:
+    print("Error while connecting to PostgreSQL", error)
+finally:
+    if (connection):
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
